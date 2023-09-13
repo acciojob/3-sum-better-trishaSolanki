f96198f295d0bcaddc5b0239e6f9f7e077814769
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-09-13 16:12:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function threeSum(arr, target) {
-// write your code here
+// write your code here let start = i+1,end = nums.length -1;
+ while(start < end){
+       const sum = nums[i]+nums[start]+nums[end];
+     if(sum === 0){
+         ans.push([nums[i],nums[start],nums[end]]);
+         start++;
+         end--;
+         while(start<end && nums[start]===nums[start-1]) start++;
+         while(start<end && nums[end]=== nums[end+1])end-=1;
+     }
+     else if(sum < 0) start++;
+     else if(sum>0) end--;
+     }
   
 }
 
